208f64068c5ed84344b9864443e445db7810b10d
DarkSideDK <37839994+DarkSideDK@users.noreply.github.com>
2019-05-21 12:46:27 +0200

    Endelig opdatering

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,28 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+/// Slideshow ///
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
+var myIndex = 0;
+carousel();
 
-function showDivs(n) {
+function carousel() {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
-  x[slideIndex-1].style.display = "block";  
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+/// Overlay Menu ///
+
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
 }
